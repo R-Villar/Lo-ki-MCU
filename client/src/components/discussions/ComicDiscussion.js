f2fc2b3949863c022f1f1f44804daa5a9b49b539
Remberto Villar <rembertovillar@gmail.com>
2022-09-28 17:06:19 -0400

    updated

--- client/src/components/discussions/ComicDiscussion.js
+++ client/src/components/discussions/ComicDiscussion.js
@@ -1,9 +1,5 @@
 import { useState, useEffect } from 'react'
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import {CardActionArea} from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
